d14aadac02d148db5ce0e51bce0ae85652f42e74
delphigkikas <funkyfisch@gmail.com>
2018-04-07 23:50:21 +0200

    Added build script

--- electron/main.js
+++ electron/main.js
@@ -2,14 +2,21 @@
 
 var app = require('app');
 var BrowserWindow = require('browser-window');
+var ipc = require('ipc');
 
 var mainWindow = null;
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
-        height: 600,
+        resizable: false,
+        frame: false,
+        height: 450,
         width: 800
     });
 
     mainWindow.loadUrl('file://' + __dirname + '/src/index.html');
+});
+
+ipc.on('close-main-window', function () {
+  app.quit();
 });
